7056cc4788787ed304c59e5e93d6005aed0cdbcc
Kevin Jiang <k.jiang@yale.edu>
2016-07-22 19:59:18 -0400

    working email sender to andover and gmail

--- simple_email.py
+++ simple_email.py
@@ -1,36 +1,23 @@
-#!/usr/bin/env
-
-'''
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("andoverace10@gmail.com", "gobigblue")
- 
-msg = "YOUR MESSAGE!"
-server.sendmail("barackobama@gmail.com", "kevjiang1@gmail.com", msg)
-server.quit()
-'''
-
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
- 
- 
+
 fromaddr = "andoverace10@gmail.com"
-fakefromaddr = 'bo@gmail.com'
-toaddr = "kevjiang1@gmail.com"
+password = "gobigblue"
+toaddr = "kjiang@andover.edu"
+subject = "Sample Subject"
+body = "Sample body"
+
 msg = MIMEMultipart()
-msg['From'] = fakefromaddr
+msg['From'] = "fromaddr"
 msg['To'] = toaddr
-msg['Subject'] = "Very Important"
+msg['Subject'] = subject
  
-body = "Please contact imemdiately"
 msg.attach(MIMEText(body, 'plain'))
  
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login(fromaddr, "gobigblue")
+server.login(fromaddr, password)
 text = msg.as_string()
-server.sendmail(fakefromaddr, toaddr, text)
+server.sendmail(fromaddr, toaddr, text)
 server.quit()
